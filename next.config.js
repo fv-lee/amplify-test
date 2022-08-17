288f6c040fc5e8f7c98ae5a0f17b9d9b4485e4c4
イヨンウ <lee.yong.woo@arsaga.jp>
2022-08-17 18:09:16 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID,
+    S3_UPLOAD_REGION: process.env.S3_UPLOAD_REGION,
+    S3_UPLOAD_BUCKET: process.env.S3_UPLOAD_BUCKET,
+  },
 };
 
 module.exports = nextConfig;
